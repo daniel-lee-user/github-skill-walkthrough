a72e3d5138f6e4cc778fa5937710999bcb4b77ef
daniel-lee-user <danielhlee02@gmail.com>
2022-02-02 23:56:04 -0800

    Update AddFunctionTest.java
    Fixed error in the test

--- AddFunctionTest.java
+++ AddFunctionTest.java
@@ -6,9 +6,13 @@ public class AddFunctionTest {
     // need to think of some tests for addfunction (specifically one that doesn't work)
     @Test
     public void testAdd() {
-        assertEquals(5, AddFunction.add(1, 3));
+        assertEquals(5, AddFunction.add(1, 4));
         // should fail because 5 != 4
     }
 } // switched the command line run command because I am on a windows machine
 // one failure confirmed out of one test.
 // time to push
+// now its time to login with ssh
+// had command saved with up key
+// dont need to change commands because server runs linux
+// time to fix code
